019d8fc8008bc1bd3ff98a7fbb60d6dffbc599b9
Conner Chyung <chyungspice@gmail.com>
2017-10-18 10:59:27 -0700

    update middleware declaration

--- trollow/settings/production.py
+++ trollow/settings/production.py
@@ -4,6 +4,4 @@ import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-MIDDLEWARE += [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-]
+MIDDLEWARE.insert(0, 'whitenoise.middleware.WhiteNoiseMiddleware') # Insert static files middleware
