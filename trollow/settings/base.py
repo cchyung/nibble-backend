345051431b262b3f33e202611a23b808feca4105
Conner Chyung <chyungspice@gmail.com>
2017-10-18 10:19:40 -0700

    Temp

--- trollow/settings/base.py
+++ trollow/settings/base.py
@@ -115,6 +115,16 @@ AUTH_PASSWORD_VALIDATORS = [
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+# Static Files (For Admin)
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
--- trollow/wsgi.py
+++ trollow/wsgi.py
@@ -15,4 +15,4 @@ from whitenoise.django import DjangoWhiteNoise
 # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trollow.settings.production")
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+# application = DjangoWhiteNoise(application)
